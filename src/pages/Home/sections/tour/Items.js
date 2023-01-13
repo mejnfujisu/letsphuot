7d1510d35d481819271a178afac242767211fec6
Vu Pham <vup.pham@terralogic.com>
2023-01-13 14:57:32 +0700

    updated

--- src/pages/Home/sections/tour/Items.js
+++ src/pages/Home/sections/tour/Items.js
@@ -3,36 +3,38 @@ import "pages/Home/sections/tour/Items.css";
 import Card from "pages/Home/sections/tour/Card";
 import Carousel from "pages/Home/sections/tour/Carousel";
 
+import ImageH from "../../../../assets/images/background/global.jpg"
+
 function items() {
   const cards = [
     {
       key: '123',
       content: (
-        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />
+        <Card imagen={ImageH} />
       )
     },
     {
       key: '124',
       content: (
-        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />
+        <Card imagen={ImageH} />      
       )
     },
     {
       key: '125',
       content: (
-        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png" />
+        <Card imagen={ImageH} />
       )
     },
     {
       key: '126',
-      content: (
-        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png" />
+      content: (        
+        <Card imagen={ImageH} />
       )
     },
     {
       key: '127',
       content: (
-        <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2019/01/pwa_880_660.jpg" />
+        <Card imagen={ImageH} />
       )
     }
   ];
